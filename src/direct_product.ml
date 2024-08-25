312e1bde91c26a27271cc9080ee98c786bb87fc6
Yaron M. Minsky <yminsky@gmail.com>
2024-08-25 09:59:55 -0400

    _

---
+++ src/direct_product.ml
@@ -0,0 +1,14 @@
+open! Base
+open Group
+
+module Direct_product (G1 : GROUP) (G2 : GROUP) : GROUP = struct
+  type t = G1.t * G2.t
+
+  let identity = G1.identity, G2.identity
+  let equal (a1, a2) (b1, b2) = G1.equal a1 b1 && G2.equal a2 b2
+  let multiply (a1, a2) (b1, b2) = G1.multiply a1 b1, G2.multiply a2 b2
+  let inverse (a1, a2) = G1.inverse a1, G2.inverse a2
+  let structure = Group_structure.cross_product G1.structure G2.structure
+  let elements = List.cartesian_product G1.elements G2.elements
+  let order = List.length elements
+end
