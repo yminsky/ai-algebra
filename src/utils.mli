14a01c8267482c0a9a62a37f6745363a7c2cbea8
Yaron M. Minsky <yminsky@gmail.com>
2024-08-25 21:22:57 -0400

    _

--- src/utils.mli
+++ src/utils.mli
@@ -8,3 +8,15 @@ val factorial : int -> int
     @param start The starting index of the subarray (inclusive)
     @param stop The ending index of the subarray (inclusive) *)
 val reverse_subarray : 'a array -> int -> int -> unit
+
+(** Calculate the parity of a permutation using inversion count
+
+    This function counts the number of inversions in a given permutation.
+    An inversion is a pair of elements (i, j) where i < j, but i appears
+    after j in the permutation.
+
+    The algorithm uses a modified merge sort approach with O(n log n) time complexity.
+
+    @param arr The input array representing the permutation
+    @return The total number of inversions in the permutation *)
+val count_inversions : int array -> int
--- test/utils_test.ml
+++ test/utils_test.ml
@@ -25,10 +25,10 @@ let%expect_test "reverse_subarray" =
   let test_reverse_subarray length start finish =
     let arr = Array.init length ~f:Int.succ in
     let original = Array.copy arr in
-    let range_marker = Array.create ~len:length '-' in
-    for i = start to finish do
-      range_marker.(i) <- '#'
-    done;
+    let range_marker =
+      Array.init length ~f:(fun i ->
+        if i >= start && i <= finish then '#' else '-')
+    in
     Utils.reverse_subarray arr start finish;
     print_s [%sexp (original : int array)];
     print_s [%sexp (range_marker : char array)];
@@ -57,5 +57,33 @@ let%expect_test "reverse_subarray" =
     (1 2 3 4 5)
     (# # # # -)
     (4 3 2 1 5)
+    |}];
+  (* an example where the subarray is empty *)
+  test_reverse_subarray 5 2 1;
+  [%expect {|
+    (1 2 3 4 5)
+    (- - - - -)
+    (1 2 3 4 5)
     |}]
+;;
+
+let%expect_test "count_inversions" =
+  let test arr =
+    let inversions = Utils.count_inversions arr in
+    print_s [%sexp (inversions : int)]
+  in
+  test [||];
+  [%expect {| 0 |}];
+  test [| 1 |];
+  [%expect {| 0 |}];
+  test [| 1; 2; 3; 4; 5 |];
+  [%expect {| 0 |}];
+  test [| 5; 4; 3; 2; 1 |];
+  [%expect {| 10 |}];
+  test [| 2; 4; 1; 3; 5 |];
+  [%expect {| 3 |}];
+  test [| 1; 1; 1; 1 |];
+  [%expect {| 0 |}];
+  test (Array.init 10 ~f:(fun i -> 10 - i));
+  [%expect {| 45 |}]
 ;;
